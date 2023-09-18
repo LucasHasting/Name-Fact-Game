f58d7f3a6ebc735dbf356c61dfe6f7f0ec23248f
LucasHasting <110124874+LucasHasting@users.noreply.github.com>
2023-09-18 17:59:35 -0500

    Add files via upload

---
+++ scripts/game_control.js
@@ -0,0 +1,73 @@
+$(document).ready(click_button)
+
+function click_button(){
+  $("#button").on("click", button);
+}
+
+function button() {
+  // get name
+  let name = $("#name").val();
+
+  let score = get_value(name);
+  let anagram = get_fact(score, name);
+
+  // update the spirit animal
+  $("#spirit_animal").text(decide_spirit_animal(score) + " and your score is " + score);
+  $("#fact").text("Fun fact: an anagram of your name is " + anagram);
+}
+
+function get_value(name) {
+  let value = 0;
+  name = name.toLowerCase();
+
+  for (let i = 0; i < name.length; i++) {
+    value += name.charCodeAt(i);
+  }
+
+  return value;
+}
+
+function decide_spirit_animal(value) {
+  let text = "";
+
+  if (value == 0) {
+    text = "Your spirit animal is a skunk";
+  }
+  else if (value < 300) {
+    text = "Your spirit animal is a tiger";
+  }
+  else if (value < 700) {
+    text = "Your spirit animal is a giraph";
+  }
+  else if (value < 1300) {
+    text = "Your spirit animal is a dragon";
+  }
+  else if (value < 1600) {
+    text = "Your spirit animal is a leopard";
+  }
+  else {
+    text = "Your spirit animal is a wolf";
+  }
+
+  return text;
+}
+
+function get_fact(score, name) {
+  let anagram = name;
+  anagram = anagram.toLowerCase();
+
+  if (score == 0) {
+    return "idiot";
+  }
+
+  for (let i = 0; i < 100000; i++) {
+    value1 = Math.floor(Math.random() * (name.length - 0));
+    value2 = Math.floor(Math.random() * (name.length - 0));
+    let arr = anagram.split('');
+    let temp = arr[value1];
+    arr[value1] = arr[value2];
+    arr[value2] = temp;
+    anagram = arr.join('');
+  }
+  return anagram;
+}
